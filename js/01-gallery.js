b275d641f4a555dda5a0c2b49a6b7fb9326ca9d4
yampolskyi10 <bizi34q@gmail.com>
2023-10-03 13:31:14 +0200

    ladybug

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+export const galleryItems = [
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+      description: 'Hokkaido Flower',
+    },
+    
+  ];
+  
+ 
+  const galleryContainer = document.querySelector('.gallery');
+  
+ 
+  const galleryMarkup = galleryItems.map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+      </a>
+    </li>
+  `).join('');
+  
+  
+  galleryContainer.innerHTML = galleryMarkup;
+  
+  
+  galleryContainer.addEventListener('click', e => {
+    e.preventDefault();
+  
+   
+    if (e.target.nodeName === 'IMG') {
+      const source = e.target.dataset.source; 
+  
+      
+      const instance = basicLightbox.create(`
+        <img src="${source}" alt="${e.target.alt}" />
+      `);
+  
+      instance.show();
+    }
+  });
+  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryItems = [
+    {
+        src: 'large-image-1.jpg',
+        alt: 'Image 1 description',
+    },
+    {
+        src: 'large-image-2.jpg',
+        alt: 'Image 2 description',
+    },
+    
+];
+
+
+function createGalleryMarkup(items) {
+    return items.map(({ src, alt }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${src}">
+                <img class="gallery__image" src="${src.replace('large-', 'small-')}" alt="${alt}" />
+            </a>
+        </li>
+    `).join('');
+}
+
+
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
