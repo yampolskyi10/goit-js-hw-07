05463720bca91eb62626f0b71975394a0c5c7e21
yampolskyi10 <bizi34q@gmail.com>
2023-10-07 17:17:06 +0200

    ladybug

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,18 @@
 
+import { galleryItems } from './gallery-items.js';
 
-export const galleryItems = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    
-  ];
-  
+
+
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `).join('');
+}
+
  
   const galleryContainer = document.querySelector('.gallery');
   
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,4 @@
 
-import galleryItems from './gallery-items.js';
-
-
-function createGalleryMarkup(items) {
-    return items.map(({ src, alt }) => `
-        <li class="gallery__item">
-            <a class="gallery__link" href="${src}">
-                <img class="gallery__image" src="${src.replace('large-', 'small-')}" alt="${alt}" />
-            </a>
-        </li>
-    `).join('');
-}
 
 
 const gallery = document.querySelector('.gallery');
