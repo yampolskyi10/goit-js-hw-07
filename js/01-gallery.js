ec9a791a9642c6e5035b73aaa35addebd7b77363
yampolskyi10 <bizi34q@gmail.com>
2023-10-08 14:47:30 +0200

    ladybug

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,4 @@
-
 import { galleryItems } from './gallery-items.js';
-
-
-
-function createGalleryMarkup(items) {
-    return items.map(({ preview, original, description }) => `
-        <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
-            </a>
-        </li>
-    `).join('');
-}
 
  
   const galleryContainer = document.querySelector('.gallery');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,13 @@
-
-
+import { galleryItems } from './gallery-items.js';
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `).join('');
+}
 
 const gallery = document.querySelector('.gallery');
 gallery.innerHTML = createGalleryMarkup(galleryItems);
